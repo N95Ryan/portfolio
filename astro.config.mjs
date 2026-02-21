42f1421661c749997d1779190b5722e4c05e73b3
Ryan Pina-silasse <ryanpinas77@gmail.com>
2026-02-21 21:35:59 +0100

    from hybrid to server

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import vercel from '@astrojs/vercel/serverless';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.ryan-pina.dev',
-  output: 'hybrid',
+  output: 'server',
   adapter: vercel(),
   integrations: [
     tailwind(), 
