68f486c4665a94d5a7837afb00965327e066b698
N95Ryan <n95jsryan@gmail.com>
2026-05-27 14:33:18 -0400

    feat/portfolio : 'refresh projects, copy and experience content'

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -3,22 +3,22 @@ import type { Experience } from '@types/index';
 export const experiences: Experience[] = [
   {
     id: 'midbec-2026',
-    location: 'Drummondville, Quebec, Canada (Remote)',
+    location: 'Drummondville, Quebec, Canada',
     technologies: ['Go', 'Chi', 'Next.js', 'PostgreSQL', 'LXC'],
   },
   {
     id: 'neat-2025',
-    location: 'Paris, France (Remote)',
+    location: 'Paris, France',
     technologies: ['Astro', 'WordPress', 'Cloudinary', 'Playwright'],
   },
   {
     id: 'konbini-2022',
-    location: 'Paris, France (Hybrid)',
+    location: 'Paris, France',
     technologies: ['Next.js', 'GraphQL', 'WordPress', 'Tailwind CSS'],
   },
   {
     id: 'french-innov-2020',
-    location: 'Stains, France (On-site)',
+    location: 'Stains, France',
     technologies: ['JavaScript', 'React', 'Bootstrap'],
   },
 ];
